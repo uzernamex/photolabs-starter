6b23561e577bc25d88387224cfb83dcce2ec8c67
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2024-01-12 09:37:46 -0500

    Remove all console logs

--- frontend/src/hooks/useApplicationData.jsx
+++ frontend/src/hooks/useApplicationData.jsx
@@ -31,8 +31,6 @@ const reducer = (state, action) => {
         };
       }
     case ACTIONS.SET_PHOTOS:
-      console.log(action.payload);
-
       return { ...state, photos: action.payload };
 
     case ACTIONS.SET_SELECTED_PHOTO:
@@ -103,16 +101,13 @@ const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   // ACTION CREATORS
-  
+
   const handleTopicClick = async (topicId) => {
     fetch(`http://localhost:8001/api/topics/photos/${topicId}`)
       .then((response) => {
-        console.log("response received");
         return response.json();
       })
       .then((photoData) => {
-        console.log("dispatching action");
-        console.log(photoData);
         dispatch({ type: ACTIONS.SET_PHOTOS, payload: photoData });
       });
   };
@@ -148,44 +143,6 @@ const useApplicationData = () => {
       console.error(`Topic with id ${topicId} not found.`);
     }
   };
-
-  ////????????
-  // const onTopicClick = (topicId) => {
-  //   dispatch({ type: ACTIONS.ON_TOPIC_CLICK, payload: { topicId } });
-  // };
-
-  // onTopicClick(topics.ACTIONS.id);
-
-  // const onLoadTopic = (topicData) => {
-  //   dispatch({ type: ACTIONS.ON_LOAD_TOPIC, payload: topicData });
-  // };
-
-  // const onLoadTopic = async (topicId) => {
-  //   dispatch({ type: ACTIONS.ON_LOAD_TOPIC });
-  //   try {
-  //     const photosForTopic = await fetchPhotosForTopic(topicId);
-  //     dispatch({
-  //       type: ACTIONS.FETCH_PHOTOS_FOR_TOPIC,
-  //       payload: { topicId, photosForTopic },
-  //     });
-  //   } catch (error) {
-  //     console.error("Error loading topic:", error);
-  //   }
-  // };
-  // const fetchPhotosForTopic = async (topicId) => {
-  //   try {
-  //     const response = await fetch(
-  //       `http://localhost:8001/api/topics/photos/${topicId}`
-  //     );
-  //     const data = await response.json();
-  //     return data;
-  //   } catch (error) {
-  //     console.error("Error fetching photos:", error);
-  //     throw error;
-  //   }
-  // };
-  console.log("state in application");
-  console.log(state.photos);
 
   useEffect(() => {
     fetch("http://localhost:8001/api/photos")
